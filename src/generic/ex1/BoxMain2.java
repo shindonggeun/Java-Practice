1333775e76be583db08876e148d1b6ff47bf2925
tlsehdrms124 <tlsehdrms124@naver.com>
2025-01-01 15:06:15 +0900

    feat: git ignore 추가 및 중급 1편 - 다형성을 통한 중복 해결 시도

---
+++ src/generic/ex1/BoxMain2.java
@@ -0,0 +1,21 @@
+package generic.ex1;
+
+public class BoxMain2 {
+
+    public static void main(String[] args) {
+        ObjectBox integerBox = new ObjectBox();
+        integerBox.setValue(10);
+        Integer integer = (Integer) integerBox.getValue();; // 다운 캐스팅 (Object -> Integer 캐스팅)
+        System.out.println("integer = " + integer);
+        
+        ObjectBox stringBox = new ObjectBox();
+        stringBox.setValue("hello");
+        String str = (String) stringBox.getValue(); // 다운 캐스팅 (Object -> String 캐스팅)
+        System.out.println("str = " + str);
+
+        // 잘못된 타입의 인수 전달시
+        integerBox.setValue("문자100");
+        Integer result = (Integer) integerBox.getValue(); // String -> Integer 캐스팅 예외 (Runtime Exception - ClassCastException)
+        System.out.println("result = " + result);
+    }
+}
