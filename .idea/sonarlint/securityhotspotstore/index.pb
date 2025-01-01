d2ff78d2d978712871eeeb03d9fef408f194183b
tlsehdrms124 <tlsehdrms124@naver.com>
2025-01-01 14:49:33 +0900

    feat: 프로젝트 초기 세팅 및 중급 1편 - 제네릭이 필요한 이유

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,11 @@
+
+=
+src/Main.java,9\2\924f089edb4af5ee45c8a9d1129cd28e2b368138
+9
+	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+O
+src/generic/ex1/IntegerBox.java,9\0\902946e4bdfe7ff3ce2e818d5ceb7ffdb716aa25
+N
+src/generic/ex1/StringBox.java,c\e\ce2341376e7f8f4007db81031184cc7516262c65
+M
+src/generic/ex1/BoxMain1.java,1\1\11cfeee39c2806590df76adbb40bba2506266b25
---
+++ src/generic/ex1/BoxMain1.java
@@ -0,0 +1,16 @@
+package generic.ex1;
+
+public class BoxMain1 {
+
+    public static void main(String[] args) {
+        IntegerBox integerBox = new IntegerBox();
+        integerBox.setValue(10); // 오토 박싱
+        Integer integer = integerBox.getValue();
+        System.out.println("integer = " + integer);
+
+        StringBox stringBox = new StringBox();
+        stringBox.setValue("hello");
+        String str = stringBox.getValue();
+        System.out.println("str = " + str);
+    }
+}
