85ebb391323b9bd065007786eb39ba2f0365fed5
tlsehdrms124 <tlsehdrms124@naver.com>
2025-01-25 23:03:09 +0900

    feat: 중급 2편 섹션 3 (제네릭 - Generic2) - 타입 매개변수 제한3 - 제네릭 도입과 실패

---
+++ src/mid2/generic/test/ex3/AnimalHospitalMainV2.java
@@ -0,0 +1,17 @@
+package mid2.generic.test.ex3;
+
+import mid2.generic.animal.Cat;
+import mid2.generic.animal.Dog;
+
+public class AnimalHospitalMainV2 {
+
+    public static void main(String[] args) {
+        AnimalHospitalV2<Dog> dogHospital = new AnimalHospitalV2<>();
+        AnimalHospitalV2<Cat> catHospital = new AnimalHospitalV2<>();
+
+        // 문제 발생: Integer, Object 같은 동물과 전혀 관계 없는 타입을 타입 인자로 전달할 수 있는 점
+        AnimalHospitalV2<Integer> integerHospital = new AnimalHospitalV2<>();
+        AnimalHospitalV2<Object> objectHospital = new AnimalHospitalV2<>();
+    }
+
+}
