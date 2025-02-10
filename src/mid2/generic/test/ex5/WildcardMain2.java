80bb064e941088b122de0c79b1dd5c93b1e55178
tlsehdrms124 <tlsehdrms124@naver.com>
2025-02-11 00:25:38 +0900

    feat: 중급 2편 섹션 3 (제네릭 - Generic2) - 와딜드카드2

---
+++ src/mid2/generic/test/ex5/WildcardMain2.java
@@ -0,0 +1,29 @@
+package mid2.generic.test.ex5;
+
+import mid2.generic.animal.Animal;
+import mid2.generic.animal.Cat;
+import mid2.generic.animal.Dog;
+
+public class WildcardMain2 {
+
+    public static void main(String[] args) {
+        Box<Object> objBox = new Box<>();
+        Box<Animal> animalBox = new Box<>();
+        Box<Dog> dogBox = new Box<>();
+        Box<Cat> catBox = new Box<>();
+
+        // Animal 포함 상위 타입 전달 가능
+        writeBox(objBox);
+        writeBox(animalBox);
+//        writeBox(dogBox); // 하한이 Animal
+//        writeBox(catBox); // 하한이 Animal
+
+        Animal animal = animalBox.get();
+        System.out.println("animal = " + animal);
+    }
+
+    // 하한 와일드 카드 (참고: 하한은 제네릭 타입, 제네릭 메서드에는 쓸 수 없음)
+    static void writeBox(Box<? super Animal> box) {
+        box.set(new Dog("멍멍이", 100));
+    }
+}
